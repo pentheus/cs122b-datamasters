4d64c5fe5532e5844c11c82736eaf53ceabc8829
scrabbleship <scrabbleship@eaa089b0-23eb-11de-982c-e375d58577e7>
2009-05-01 08:03:44 +0000

    Importing new stuff.

---
+++ trunk/Phase2/src/info/Login.java
@@ -0,0 +1,14 @@
+package info;
+
+public class Login 
+{
+	private String info = "Here is the info";
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+}
